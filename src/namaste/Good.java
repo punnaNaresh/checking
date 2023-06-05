9403db3d01e7a6013b89a3abb8064f8f2675089d
naresh <nareshpunna555@gmail.com>
2023-06-05 16:03:20 +0530

    56745 punna naresh code commits

---
+++ src/namaste/Good.java
@@ -0,0 +1,12 @@
+package namaste;
+
+public class Good {
+
+	public static void main(String[] args) 
+	{
+	System.out.println("thanks for asking.........");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/namaste/Welcome.java
@@ -0,0 +1,13 @@
+package namaste;
+
+public class Welcome {
+
+	public static void main(String[] args)
+	{
+	
+	System .out.println("your welcome...");
+		// TODO Auto-generated method stub
+
+	}
+
+}
